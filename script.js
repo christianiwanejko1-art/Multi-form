e8e5aabb1d62fe787fff3f206cc8c2d447f37f3f
Christian Iwanejko <christianiwanejko1-art>
2026-01-26 17:43:54 +0000

    added appropriate colours and added form validation

--- script.js
+++ script.js
@@ -7,21 +7,23 @@ let customer = []
 const step1btn = document.getElementById('submit1');
 const step2btn = document.getElementById('submit2');
 
-const step1form = document.getElementById('step1Form');
+const step1Form = document.getElementById('step1Form');
 // step1btn.addEventListener('click',(e)=>{
 //     e.preventDefault();
 //     console.log('btn pressed');
 // })
 
-step1form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(step1form);
+step1Form.addEventListener('submit', (e) => {
+e.preventDefault();
+if (!step1Form.checkValidity()) return;
+    const formData = new FormData(step1Form);
     const data = {
         name: formData.get('name'),
         email: formData.get('email'),
         phone: formData.get('phone')
     }
     customer.push(data);
+    console.log(customer)
     step1.classList.add('hidden');
     step2.classList.remove('hidden');
 })
@@ -40,3 +42,56 @@ backbtn.addEventListener('click', () => {
   steps[currentIndex - 1].classList.remove('hidden');
 });
 
+const email1 = document.getElementById('email');
+const name1 = document.getElementById('name');
+const phone1 = document.getElementById('phone');
+
+
+
+name1.addEventListener('invalid',()=>{
+  nameError.textContent = 'This field is required';
+  name1.style.border = '1px solid hsl(354, 84%, 57%)';
+})
+
+name1.addEventListener('input',()=>{
+  if (name1.checkValidity()){
+    nameError.textContent = '';
+    name1.style.border = '1px solid hsl(213, 96%, 18%);'
+  }
+})
+
+email1.addEventListener('invalid',()=>{
+  emailError.textContent = 'This field is required';
+  email1.style.border = '1px solid hsl(354, 84%, 57%)';
+})
+
+email1.addEventListener('input',()=>{
+  if (email1.checkValidity()){
+    emailError.textContent = '';
+    email1.style.border = '1px solid hsl(213, 96%, 18%)';
+  }
+})
+
+phone1.addEventListener('invalid',()=>{
+  phoneError.textContent = 'This field is required';
+  phone1.style.border = '1px solid hsl(354, 84%, 57%)';
+})
+
+phone1.addEventListener('input',()=>{
+  if (phone1.checkValidity()){
+    phoneError.textContent = '';
+    phone1.style.border = '1px solid hsl(213, 96%, 18%);'
+  }
+})
+
+const cardGroup = document.getElementById('plans');
+
+cardGroup.addEventListener('click', (e) => {
+  const card = e.target.closest('.plan');
+  if (!card) return;
+  console.log(card);
+  cardGroup.querySelectorAll('.plan').forEach((c)=>{
+    c.classList.remove('active');
+  })
+  card.classList.add('active');
+})
